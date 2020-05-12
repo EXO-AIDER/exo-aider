614cebe4cb46b0998816cf02de17140cc38d2cd3
Jacob Nørgaard <jacobnoergaard94@gmail.com>
2020-05-12 15:21:22 +0200

    added real time plotting to matlab

--- esp32_new_bluetooth/Matlab/Tests/SppBluetooth_simsig.m
+++ esp32_new_bluetooth/Matlab/Tests/SppBluetooth_simsig.m
@@ -147,7 +147,7 @@ plot(t_n, u_n);
 legend('u');
 
 %% Read sensors, move "function" into section
-my_sensor_band = SppBluetooth('0877A9E350CC@Exo-Aider', 'my_left_sensor_band_task', 60 * 1000);
+my_sensor_band = SppBluetooth('0877A9E350CC@Exo-Aider', 'my_left_sensor_band_task', 60 * 1000); % buffer size at the end
 
     my_sensor_band.sample_frequency = 1000;
     my_sensor_band.send_signals_ratio = 40;
@@ -157,10 +157,26 @@ my_sensor_band = SppBluetooth('0877A9E350CC@Exo-Aider', 'my_left_sensor_band_tas
     signals_got = min(my_sensor_band.signal_n);
     % my_sensor_band.send_signals = false;
     
-FSR = my_sensor_band.get_signals({'FSR1','FSR2','FSR3','FSR4','FSR5','FSR6','FSR7','FSR8'}, -9:0);
-EMG1 = my_sensor_band.get_signals('EMG1','EMG2','EMG3','EMG4');
-IMU1 = my_sensor_band.get_signals('AccX1', 'AccY1', 'AccZ1', 'GyroX1', 'GyroY1', 'GyroZ1'); 
-IMU2 = my_sensor_band.get_signals('AccX2', 'AccY2', 'AccZ2', 'GyroX2', 'GyroY2', 'GyroZ2');
+FSR = my_sensor_band.get_signals({'FSR1','FSR2','FSR3','FSR4','FSR5','FSR6','FSR7','FSR8'});
+EMG = my_sensor_band.get_signals({'EMG1','EMG2','EMG3','EMG4'});
+IMU1 = my_sensor_band.get_signals({'AccX1', 'AccY1', 'AccZ1', 'GyroX1', 'GyroY1', 'GyroZ1'}); 
+IMU2 = my_sensor_band.get_signals({'AccX2', 'AccY2', 'AccZ2', 'GyroX2', 'GyroY2', 'GyroZ2'});
+
+%% Real time plotting
+
+x = linspace(0, 60, 60000);
+
+for i = 1:1000 % plotting in 100 seconds real time
+    FSR = my_sensor_band.get_signals({'FSR1','FSR2','FSR3','FSR4','FSR5','FSR6','FSR7','FSR8'});
+    EMG = my_sensor_band.get_signals({'EMG1','EMG2','EMG3','EMG4'});
+    IMU1 = my_sensor_band.get_signals({'AccX1', 'AccY1', 'AccZ1', 'GyroX1', 'GyroY1', 'GyroZ1'}); 
+    IMU2 = my_sensor_band.get_signals({'AccX2', 'AccY2', 'AccZ2', 'GyroX2', 'GyroY2', 'GyroZ2'});
+    
+    plot(x+i*0.1, FSR);
+    xlabel('Seconds')
+    
+    pause(0.1);
+end
 
 
 
